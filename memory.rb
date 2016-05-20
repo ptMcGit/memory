12877bbf228954446a9e21a7e59d48a3c7c135ec
Zach H <zdhadgraft@gtcc.edu>
2016-05-20 12:40:07 -0400

    Improved card presentation.

--- memory.rb
+++ memory.rb
@@ -40,22 +40,10 @@ def load_objects
   ["a", "b", "c", "d"]
 end
 
-def combine_hashes(hash1, hash2)
-  hash2 = hash2.clone
-  hash1.each do |key,value|
-    if value
-      hash2[key] = value
-    end
-  end
-  return hash2
-end
-
 def guesses_good(board, turn_guesses)
   x = Hash[board.select { |x, y| turn_guesses.include? x }].map {|x,y| y}
   x[0] == x[1]
 end
-
-
 
 # def resolve_guesses(board, turn_guesses)
 #   b = {}
@@ -73,19 +61,6 @@ end
 #   binding.pry
 # end
 
-# def reverse_guesses(board, items)
-#   binding.pry
-# end
-  
-
-#def guesses_good(items)
-#  reverse_lookup(items[0]) == reverse_lookup(items[0])
-#end
-  
-  
-
-
-  
 #  items.uniq.count == 1
 #end  
   
@@ -103,13 +78,6 @@ end
 #  guesses.map { |x,y| y }.uniq.count == 1
 #end
 
-
-def create_guess_board(board)
-  # send in an array and get back a hash
-  Hash[board.to_a.map { |x| [x[0],nil] }]
-end
-
-
 def print_board(board, correct_guesses, turn_guesses)
   size = board.count
   size_of_block = board.to_a.last[0].to_s.length
@@ -117,9 +85,9 @@ def print_board(board, correct_guesses, turn_guesses)
   
   board.map { |key, value| key }.each do |x|
     if (correct_guesses + turn_guesses).include? x
-      printf("%.2s ", board[x])
+      print " [", (board[x].center size_of_block), "] "
     else
-      printf("%.2s ", x.to_s)
+      print " [", (x.to_s.center size_of_block), "] "
     end
     if count % print_new_line(size) == 0
       
@@ -128,16 +96,6 @@ def print_board(board, correct_guesses, turn_guesses)
     count += 1
   end
   print "\n"
-
-  #board.to_a.each do |x|
-
-   # printf("%.2s ", x[1] || x[0].to_s)
-
-    #if count % print_new_line(size) == 0
-     # print "\n"
-    #end
-    #count += 1
-
 end
 
 
@@ -159,24 +117,6 @@ end
 #   end
 #     return guess + 1
 # end
-
-#binding.pry
-
-
-#def synthesize_board(board1, board2)
-
-  #result = (board1.to_a.select do |x|
-   #           x[1]
-    #        end) + (board2.to_a.select do |x|
-     #                 x[1]
-      #              end)
-
-#  board = create_board
-#  board1.select do |x|
-
-
-#  binding.pry
-#end
 
 
 def  get_valid_input(valid_input)
